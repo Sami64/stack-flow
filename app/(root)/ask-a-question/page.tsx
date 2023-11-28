7f8c92d86f4e0a69802f9c04e85f58c3fa8ebc2d
Samuel Paintsil <smashbros54@gmail.com>
2023-11-28 21:56:10 +0000

    🚑 set up authentication

---
+++ app/(root)/ask-a-question/page.tsx
@@ -0,0 +1,5 @@
+const AskAQuestion = () => {
+	return <div>AskAQuestion</div>
+}
+
+export default AskAQuestion
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-	return (
-		<div>
-			<h1 className="h1-bold">Home is here</h1>
-		</div>
-	)
-}
