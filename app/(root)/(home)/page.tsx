8663a80027dedfd5d14c2ff13b466ce324c7edde
Samuel Paintsil <smashbros54@gmail.com>
2023-12-03 20:20:20 +0000

    ⚡️ add theme,mobile nav

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,8 @@
-import { UserButton } from '@clerk/nextjs'
-
 const Home = () => {
 	return (
 		<div>
-			<UserButton afterSignOutUrl="/" />
+			
+			Home
 		</div>
 	)
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/context/ThemeProvider'
 import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 // eslint-disable-next-line camelcase
@@ -32,18 +33,18 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<ClerkProvider
-			appearance={{
-				elements: {
-					formButtonPrimary: 'primary-gradient',
-					footerActionLink: 'primary-text-gradient hover:text-primary-500',
-				},
-			}}>
-			<html lang="en">
-				<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-					{children}
-				</body>
-			</html>
-		</ClerkProvider>
+		<html lang="en">
+			<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+				<ClerkProvider
+					appearance={{
+						elements: {
+							formButtonPrimary: 'primary-gradient',
+							footerActionLink: 'primary-text-gradient hover:text-primary-500',
+						},
+					}}>
+					<ThemeProvider>{children}</ThemeProvider>
+				</ClerkProvider>
+			</body>
+		</html>
 	)
 }
