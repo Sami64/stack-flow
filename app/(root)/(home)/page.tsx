6162ecca9709f03eb6cb94ad2490b731be513cf0
Samuel Paintsil <smashbros54@gmail.com>
2023-12-16 13:57:39 +0000

    🎨 Add button and link to ask a question

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,22 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
 const Home = () => {
 	return (
-		<div>
-			
-			Home
-		</div>
+		<>
+			<div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+				<h1 className="h1-bold text-dark100_light900">All Questions</h1>
+				<Link
+					href="/ask-question"
+					className="flex justify-end max-sm:w-full">
+					<Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+						Ask a Question
+					</Button>
+				</Link>
+			</div>
+
+			<div></div>
+		</>
 	)
 }
 
