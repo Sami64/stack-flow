b191a4082da5b05355caf4d16f2bb8e30a6af9bd
Samuel Paintsil <smashbros54@gmail.com>
2023-12-21 20:20:20 +0000

    🚑 update local search bg

---
+++ components/shared/search/LocalSearch.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Input } from '@/components/ui/input'
+import Image from 'next/image'
+
+const LocalSearch = () => {
+	return (
+		<div className="relative w-full py-10">
+			<div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+				<Image
+					src="/assets/icons/search.svg"
+					width={24}
+					height={24}
+					alt="search"
+					className="cursor-pointer"
+				/>
+				<Input
+					type="text"
+					placeholder="Search questions..."
+					value=""
+					onChange={() => {}}
+					className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default LocalSearch
