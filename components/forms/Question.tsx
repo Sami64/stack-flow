3be9d1c7151075ba24be4192503a306da77ec8f9
Samuel Paintsil <smashbros54@gmail.com>
2024-01-15 20:20:20 +0000

    🐛 add server actions

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -11,21 +11,74 @@ import {
 	FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { createQuestion } from '@/lib/actions/question.action'
 import { QuestionsSchema } from '@/lib/validations'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { Editor } from '@tinymce/tinymce-react'
+import Image from 'next/image'
+import React, { useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
+import { Badge } from '../ui/badge'
+
+const type: any = 'create'
 
 const Question = () => {
+	const editorRef = useRef<Editor | null>(null)
+	const [isSubmitting, setIsSubmitting] = useState(false)
+
 	const form = useForm<z.infer<typeof QuestionsSchema>>({
 		resolver: zodResolver(QuestionsSchema),
 		defaultValues: {
 			title: '',
+			explanation: '',
+			tags: [],
 		},
 	})
 
-	function onSubmit(values: z.infer<typeof QuestionsSchema>) {
-		console.log(values)
+	const handleTagRemove = (tag: string, field: any) => {
+		const tags = field.value.filter((t: string) => t !== tag)
+		form.setValue('tags', tags)
+	}
+
+	const handleInputKeyDown = (
+		e: React.KeyboardEvent<HTMLInputElement>,
+		field: any
+	) => {
+		if (e.key === 'Enter' && field.name === 'tags') {
+			e.preventDefault()
+
+			const tagInput = e.target as HTMLInputElement
+			const tagValue = tagInput.value.trim()
+
+			if (tagValue !== '') {
+				if (tagValue.length > 15) {
+					return form.setError('tags', {
+						type: 'required',
+						message: 'Tag must be less than 15 characters',
+					})
+				}
+
+				if (!field.value.includes(tagValue as never)) {
+					form.setValue('tags', [...field.value, tagValue])
+					tagInput.value = ''
+					form.clearErrors('tags')
+				} else {
+					form.trigger()
+				}
+			}
+		}
+	}
+
+	async function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+		setIsSubmitting(true)
+		try {
+			// make async call to api
+			await createQuestion(values)
+		} catch (error) {
+		} finally {
+			setIsSubmitting(false)
+		}
 	}
 
 	return (
@@ -66,9 +119,43 @@ const Question = () => {
 								<span className="text-primary-500">*</span>
 							</FormLabel>
 							<FormControl className="mt-3.5">
-								<Input
-									className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-									{...field}
+								<Editor
+									apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+									onInit={(evt, editor) => {
+										// @ts-ignore
+										editorRef.current = editor
+									}}
+									initialValue=""
+									onEditorChange={(content, editor) => {
+										field.onChange(content)
+									}}
+									onBlur={(content, editor) => field.onBlur()}
+									init={{
+										height: 350,
+										menubar: false,
+										plugins: [
+											'advlist',
+											'autolink',
+											'lists',
+											'link',
+											'image',
+											'charmap',
+											'preview',
+											'anchor',
+											'searchreplace',
+											'visualblocks',
+											'fullscreen',
+											'insertdatetime',
+											'media',
+											'table',
+											'codesample',
+										],
+										toolbar:
+											'undo redo | blocks | ' +
+											'codesample | bold italic forecolor | alignleft aligncenter ' +
+											'alignright alignjustify | bullist numlist ',
+										content_style: 'body { font-family:Inter; font-size:16px }',
+									}}
 								/>
 							</FormControl>
 							<FormDescription className="body-regular mt-2.5 text-light-500">
@@ -89,10 +176,32 @@ const Question = () => {
 								Tags <span className="text-primary-500">*</span>
 							</FormLabel>
 							<FormControl className="mt-3.5">
-								<Input
-									className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-									{...field}
-								/>
+								<>
+									<Input
+										className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+										onKeyDown={(e) => handleInputKeyDown(e, field)}
+									/>
+
+									{field.value.length > 0 && (
+										<div className="flex-start mt-2.5 gap-2.5">
+											{field.value.map((tag: any) => (
+												<Badge
+													key={tag}
+													onClick={() => handleTagRemove(tag, field)}
+													className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize">
+													{tag}{' '}
+													<Image
+														src="/assets/icons/close.svg"
+														alt="close icon"
+														width={12}
+														height={12}
+														className="cursor-pointer object-contain invert-0 dark:invert"
+													/>
+												</Badge>
+											))}
+										</div>
+									)}
+								</>
 							</FormControl>
 							<FormDescription className="body-regular mt-2.5 text-light-500">
 								Add up to 5 tags to describe what your question is about. Start
@@ -102,7 +211,16 @@ const Question = () => {
 						</FormItem>
 					)}
 				/>
-				<Button type="submit">Submit</Button>
+				<Button
+					type="submit"
+					disabled={isSubmitting}
+					className="primary-gradient w-fit !text-light-900">
+					{isSubmitting ? (
+						<>{type === 'edit' ? 'Editing' : 'Posting...'}</>
+					) : (
+						<>{type === 'edit' ? 'Edit Question' : 'Ask a Question'}</>
+					)}
+				</Button>
 			</form>
 		</Form>
 	)
